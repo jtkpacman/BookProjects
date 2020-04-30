afde2ba5c40e6048786a842b276b6544f75e8d6b
Jacob <jtkenyon051992@gmail.com>
2020-04-30 17:55:43 -0400

    Finished adding items to inventory Code

--- fantastyInventory.py
+++ fantastyInventory.py
@@ -1,10 +1,25 @@
 # Author Jacob Kenyon
 # Create an inventory using a dictionary as the inventory model e.g.. {"Item name": amount}
+# PART II : Create a looting system. (EXTREMELY SIMPLIFIED)
 
-stuff = {"Rope": 3, "Torch": 7, "Knife": 2, "Soup": 3,
-         "Bread": 2, "Water": 6, "Gold Coin": 378,
-         "Bronze Coin": 1226, }
+stuff = {"Rope": 3, "Torch": 7, "Knife": 2, "Soup": 3, "Bread": 2, "Water": 6,
+         "Gold Coin": 378, "Bronze Coin": 1226, }
 
+dragon_loot = ['Gold Coin', 'Gold Coin', 'Gold Coin', 'Bronze Coin', 'Bronze Coin', 'Rope', 'Dragon Claw', 'Dragon Claw', ]
+
+
+# Takes a list of items and adds them to a dictionary containing the inventory.
+def addToInventory(inventory, loot):
+    for i in loot:
+        if i not in inventory:
+            inventory.setdefault(i, 1)
+            print("Added one NEW " + i)
+        else:
+            inventory[i] += 1
+            print("Added one " + i)
+
+
+# Display the inventory
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
@@ -14,4 +29,8 @@ def displayInventory(inventory):
         item_total += v
     print("\nTotal number of items: " + str(item_total))
 
+
+# Code to Run
+displayInventory(stuff)
+addToInventory(stuff, dragon_loot)
 displayInventory(stuff)
